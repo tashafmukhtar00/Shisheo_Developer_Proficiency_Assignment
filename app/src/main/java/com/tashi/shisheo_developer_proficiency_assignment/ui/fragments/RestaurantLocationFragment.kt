20d7a11b4ba4e0b0c6b6acf18607d9b24d9a49ef
tashi <tashafmukhtar@gmail.com>
2021-03-24 22:39:46 +0400

    implementation of user current location feature

--- app/src/main/java/com/tashi/shisheo_developer_proficiency_assignment/ui/fragments/RestaurantLocationFragment.kt
+++ app/src/main/java/com/tashi/shisheo_developer_proficiency_assignment/ui/fragments/RestaurantLocationFragment.kt
@@ -1,60 +1,250 @@
 package com.tashi.shisheo_developer_proficiency_assignment.ui.fragments
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.tashi.shisheo_developer_proficiency_assignment.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RestaurantLocationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RestaurantLocationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class RestaurantLocationFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        private const val MY_PERMISSION_CODE: Int = 1
+
     }
+
+    private var latitude: Double = 0.toDouble()
+    private var longitude: Double = 0.toDouble()
+
+    private lateinit var mLastLocation: Location
+    private var mMarker: Marker? = null
+
+
+    // Location
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+
+    private lateinit var placesClient: PlacesClient
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_location, container, false)
+        val view = inflater.inflate(R.layout.fragment_restaurant_location, container, false)
+        mapView = view.findViewById(R.id.map_view)
+
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
+
+
+            if (checkLocationPermission()) {
+
+                buildLocationRequest()
+                buildLocationCallback()
+
+
+                fusedLocationProviderClient =
+                    LocationServices.getFusedLocationProviderClient(context!!)
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()
+                )
+            } else {
+
+                buildLocationRequest()
+                buildLocationCallback()
+
+
+                fusedLocationProviderClient =
+                    LocationServices.getFusedLocationProviderClient(context!!)
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()
+                )
+
+            }
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RestaurantLocationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RestaurantLocationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun buildLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                // super.onLocationResult(p0)
+
+                mLastLocation = p0!!.locations.get(p0.locations.size - 1)  // Get last location
+
+                if (mMarker != null) {
+
+                    mMarker!!.remove()
                 }
+
+                latitude = mLastLocation.latitude
+                longitude = mLastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions()
+                    .position(latLng)
+                    .title("Your location")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
+
+                mMarker = googleMap.addMarker(markerOptions)
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(11f))
             }
+
+
+        }
     }
+
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+
+
+    }
+
+    private fun checkLocationPermission(): Boolean {
+
+
+        if (ContextCompat.checkSelfPermission(
+                activity!!,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity!!,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            )
+                ActivityCompat.requestPermissions(
+                    activity!!, arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ), MY_PERMISSION_CODE
+                )
+            else
+                ActivityCompat.requestPermissions(
+                    activity!!, arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ), MY_PERMISSION_CODE
+                )
+            return false
+        } else
+            return true
+
+    }
+
+    // override onRequestPermissionResult
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            MY_PERMISSION_CODE -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(
+                        activity!!,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                )
+                    if (checkLocationPermission())
+                        googleMap.isMyLocationEnabled = true
+
+
+                buildLocationRequest()
+                buildLocationCallback()
+
+
+                fusedLocationProviderClient =
+                    LocationServices.getFusedLocationProviderClient(context!!)
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()
+                )
+
+            } else {
+                Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map!!
+
+        // init google play services
+
+        if (ContextCompat.checkSelfPermission(
+                activity!!,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            googleMap.isMyLocationEnabled = true
+        } else {
+            googleMap.isMyLocationEnabled = true
+        }
+
+        // enable zoom control
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+
+    override fun onStop() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        super.onStop()
+    }
+
+
+    private fun nearbyRestaurants() {
+
+
+    }
+
 }
